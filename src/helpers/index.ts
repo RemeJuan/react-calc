054c0a0679b298c4e3452b2ddd8291339054efc5
remelehane <reme.lehane@gmail.com>
2021-01-01 07:27:03 +0200

    feat(logic): setup business logic

---
+++ src/helpers/index.ts
@@ -0,0 +1,32 @@
+export const handleClick = (button: string, result: string): string => {
+  switch (button) {
+    case "=":
+      return calculate(result);
+    case "C":
+      return reset();
+    case "CE":
+      return backspace(result);
+    default:
+      return result + button;
+  }
+}
+
+export const calculate = (result: string): string => {
+  let checkResult = '';
+
+  if (result.includes('--')) {
+    checkResult = result.replace('--', '+');
+  } else {
+    checkResult = result
+  }
+
+  try {
+    return (eval(checkResult) || "") + "";
+  } catch (e) {
+    return "error";
+  }
+};
+
+export const reset = () => "";
+
+export const backspace = (result: string): string => result.slice(0, -1);

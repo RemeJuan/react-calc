2433fa5899f1ebecfca82c98031fde262ff45b30
remelehane <reme.lehane@gmail.com>
2021-01-01 07:27:55 +0200

    feat(app): replaced default page with calculator layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {KeyPadComponent, ResultComponent} from './components';
+import {handleClick} from "./helpers";
 
-function App() {
+const App = () => {
+  const [result, setResult] = useState<string>('');
+
+  const onClick = (button: string) => setResult(handleClick(button, result));
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="calculator-body">
+        <h1>EE Calculator</h1>
+        <ResultComponent result={result}/>
+        <KeyPadComponent onClick={onClick}/>
+      </div>
     </div>
   );
 }
